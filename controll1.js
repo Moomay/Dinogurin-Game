fd17d1617d5b1e2a86cd168d025ac6471467d024
SarunyuRuenruay <sarunyu.n17@gmail.com>
2019-03-12 18:12:50 +0700

    update controller

--- controll1.js
+++ controll1.js
@@ -1,20 +1,34 @@
 var x = 2;
-//setInterval(function(){
- //x = Math.floor(Math.random() * 5);
-  document.body.onkeydown = function(event) {
-    console.log(event.code);
-    console.log(x);
-      if (event.code == "ArrowRight")
-        x += 1;
-      else if (event.code == "ArrowLeft")
-        x -= 1;
-      if (x >= 4){
-        x = 4
-      }
-      else if (x <= 0){
-        x = 0
-      }
-      document.querySelector('#dino').setAttribute('x',x); 
+//with keyboard
+document.body.onkeydown = function(event) {
+  console.log(event.code);
+  console.log(x);
+    if (event.code == "ArrowRight")
+      x += 1;
+    else if (event.code == "ArrowLeft")
+      x -= 1;
+    if (x >= 4){
+      x = 4;
+    }
+    else if (x <= 0){
+      x = 0;
+    }
+    document.querySelector('#dino').setAttribute('x',x); 
+}
+//with touchscreen
+document.getElementById("conl").onclick = function() {
+  console.log("click left");
+  x -= 1;
+  if (x <= 0){
+    x = 0;
   }
-  document.querySelector('#dino').setAttribute('x',x);  
-//},1000)
+  document.querySelector('#dino').setAttribute('x',x);
+}
+document.getElementById("conr").onclick = function(){
+  console.log("click right");
+  x += 1;
+  if (x >= 4){
+    x = 4;
+  }
+  document.querySelector('#dino').setAttribute('x',x);
+}
--- fruitDrop.js
+++ fruitDrop.js
@@ -1,18 +1,25 @@
-function fDrop(x){
-	document.querySelector('#fruit').setAttribute('x',x)
+function fDrop(posi_x){
+	document.querySelector('#fruit').setAttribute('x',posi_x);
+	y = -1;
 	var fdrop = setInterval(function(){
-	y += 1;
-	//var position = document.querySelector('#fruit[x]');
-	document.querySelector('#fruit').setAttribute('y',y);
-	if (y == 10){
-		clearInterval(fdrop);
-	}
+		y += 1;
+		//var position = document.querySelector('#fruit[x]');
+		document.querySelector('#fruit').setAttribute('y',y);
+		if (y == 10){
+			clearInterval(fdrop);
+			y = 0;
+			pos = Math.floor(Math.random() * 5);
+			fDrop(pos);
+		}
 	console.log(y);
 	},1000);
 }
-setInterval(function(){
-	y = 0;
-	x = Math.floor(Math.random() * 5);
-	fDrop(x);
-},10000)
 
+function myFunction() {
+  var myVar = setInterval(alertFunc, 3000);
+}
+
+function alertFunc() {
+  alert("Hello!");
+}
+fDrop(1);
