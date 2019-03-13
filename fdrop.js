e6124af682bd2467b16f8d274f4f4c0b8f1056d0
SarunyuRuenruay <sarunyu.n17@gmail.com>
2019-03-14 00:36:11 +0700

    drop meteor and hp

--- fdrop.js
+++ fdrop.js
@@ -1,57 +1,80 @@
 var score = 0;
+var hp = 10;
+var mulscore = 1;
 console.log(document.getElementById("dino").getAttribute("x"));
 function ready_go() {
   var count = 0;
-  var myVar = setInterval(random_on, 1000);
   //random_on();
-  var delay = 1000;
+  var delay_drop = 1000;
+  var delay_spawn = 1000;
+  var myVar = setInterval(random_on, delay_spawn);
   function random_on(){
-  	//create Element wiht dive
+  	//create Element with dive
+    var rate = Math.floor(Math.random() * 100);
   	var posXMeat = Math.floor(Math.random() * 5);
   	var posYMeat = -1;
   	meat = document.createElement('div');
   	meat.setAttribute("id", count);
-  	meat.setAttribute("class", "fruit");
+    if (rate >= 80){
+  	 meat.setAttribute("class", "fruit");
+    }
+    else {
+      meat.setAttribute("class", "meteor");
+    }
   	meat.setAttribute("y", posYMeat);
   	meat.setAttribute("x", posXMeat);
   	document.getElementById("content").appendChild(meat);
   	
-  	//drop meat
+  	
     var temp = count;
     var iscount = 1;
-    dorp_meat(meat ,temp, posXMeat, posYMeat, iscount);
-    function dorp_meat(meat, para, posit_x, posit_y, is_count){
-      setTimeout(function dorp(){
+    //drop meat
+    drop_meat(meat, temp, posXMeat, posYMeat, iscount);
+    function drop_meat(meat, para, posit_x, posit_y, is_count){
+      setTimeout(function drop(){
         var status = 1;
         posit_y += 1;
         //check hit
         if (posit_y >= 9 && posit_y <= 10 && status == 1 && is_count == 1){
-          if (posit_x ==  document.getElementById("dino").getAttribute("x")){
-            score += 1;
-            document.getElementById("score").innerHTML = "SCORE:"+score;
             console.log("dino hit");
-            console.log("this score: "+score);
-            is_count = 0;
+          if (posit_x ==  document.getElementById("dino").getAttribute("x")){
+            console.log(document.getElementById(para).getAttribute("class"));
+            if (document.getElementById(para).getAttribute("class") == "fruit"){
+              score += (1*mulscore);
+              document.getElementById("score").innerHTML = "SCORE: "+score;
+              console.log("this score: "+score);
+            }
+            else {
+              hp -= (1*mulscore);
+              console.log("HP -1");
+              document.getElementById("hp").innerHTML = "HP: "+hp;
+              if (hp == 0){
+                console.log("Game End");
+                mulscore = 0;
+                clearInterval(myVar);
+              }
+            }
+            is_count = 0; //no count;
           }
         }
         meat.setAttribute("y", posit_y);
         // recursive
         if (posit_y <= 10 && status == 1){
-         dorp_meat(meat, para, posit_x , posit_y, is_count);
+         drop_meat(meat, para, posit_x, posit_y, is_count);
         }
         // remove when out screen
         if (posit_y == 10){
-         remove(temp);
+         remove(para);
          status = 0;
     }
-  }, delay);
+  }, delay_drop);
   }
 
 	//count id +1
 	count += 1;
   //remove ID
   function remove(meatID){
-	var elem = document.getElementById(meatID);
+	 var elem = document.getElementById(meatID);
     return elem.parentNode.removeChild(elem);
   }
   }
